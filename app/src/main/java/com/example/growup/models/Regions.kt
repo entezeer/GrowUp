ff9fb47bf20ac1ff477d8ce59979e085e2f65be8
vanganistan <vanocpp@gmail.com>
2019-04-25 21:42:44 +0300

    fast commit with unimpotant changes
    fast commit with unimpotant changes

---
+++ app/src/main/java/com/example/growup/models/Regions.kt
@@ -0,0 +1,21 @@
+package com.example.growup.models
+
+object Regions {
+    val regions = arrayOf("Иссык-Кульская область","Чуйская область","Ошская область","Баткенская область","Нарынская область","Таласская область","Джалал-Абадская область")
+
+    val issykKul = arrayOf("Ак-Суйский район","Джети-Огузский район","Тонский район","Тюпский район","Иссык-Кульский район","г. Каракол","г. Балыкчи")
+
+    val chuy = arrayOf("")
+
+    val osh = arrayOf("")
+
+    val batken = arrayOf("")
+
+    val naryn = arrayOf("")
+
+    val talas = arrayOf("")
+
+    val jalalAbad = arrayOf("")
+
+    val regionsList = arrayOf(issykKul, chuy, osh, batken, naryn, talas, jalalAbad)
+}
