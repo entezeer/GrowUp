fbc4a290a2686695362a43f75d9a883e28fa4875
vanganistan <vanocpp@gmail.com>
2019-04-25 21:50:26 +0300

    fast commit with unimpotant changes
    fast commit with unimpotant changes

--- app/src/main/java/com/example/growup/RegisterActivity.kt
+++ app/src/main/java/com/example/growup/RegisterActivity.kt
@@ -3,9 +3,12 @@ package com.example.growup
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.view.View
 import android.widget.*
 import com.entezeer.tracking.utils.ValidUtils
 import com.example.growup.models.CountryCodes
+import com.example.growup.models.Regions
 import com.example.growup.models.User
 
 class RegisterActivity : AppCompatActivity() {
@@ -19,10 +22,14 @@ class RegisterActivity : AppCompatActivity() {
     private var number: EditText? = null
     private var password: EditText? = null
 
+    private var backButton: FloatingActionButton? = null
+
     private var userType = ""
 
     private var registerBtn: Button? = null
-    private var spinner: Spinner? = null
+    private var spinnerCountries: Spinner? = null
+    private var spinnerRegions: Spinner? = null
+    private var spinnerDistricts: Spinner? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +41,10 @@ class RegisterActivity : AppCompatActivity() {
 
     private fun init() {
         registerBtn = findViewById(R.id.register_btn)
-        spinner = findViewById(R.id.spinner_countries)
+        spinnerCountries = findViewById(R.id.spinner_countries)
         radioGroup = findViewById(R.id.type_radio_group)
+        spinnerRegions = findViewById(R.id.spinner_regions)
+        spinnerDistricts = findViewById(R.id.spinner_districts)
 
         name = findViewById(R.id.name)
         lastName = findViewById(R.id.last_name)
@@ -43,8 +52,33 @@ class RegisterActivity : AppCompatActivity() {
         number = findViewById(R.id.number)
         password = findViewById(R.id.password)
 
-        spinner?.adapter =
+        backButton = findViewById(R.id.back_button)
+
+        backButton?.setOnClickListener {
+            onBackPressed()
+        }
+
+        spinnerCountries?.adapter =
             ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, CountryCodes.countryNames)
+
+        spinnerRegions?.adapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, Regions.regions)
+
+        spinnerRegions?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                spinnerDistricts?.adapter =
+                    ArrayAdapter<String>(this@RegisterActivity,
+                        android.R.layout.simple_spinner_dropdown_item,
+                        Regions.regionsList[0])
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                spinnerDistricts?.adapter =
+                    ArrayAdapter<String>(this@RegisterActivity,
+                        android.R.layout.simple_spinner_dropdown_item,
+                        Regions.regionsList[spinnerRegions?.selectedItemPosition!!])
+            }
+        }
 
         registerBtn?.setOnClickListener {
             register()
@@ -54,7 +88,7 @@ class RegisterActivity : AppCompatActivity() {
     private fun register() {
         checkUserType()
         Toast.makeText(this, userType, Toast.LENGTH_LONG).show()
-        val code = CountryCodes.countryAreaCodes[spinner?.selectedItemPosition!!]
+        val code = CountryCodes.countryAreaCodes[spinnerCountries?.selectedItemPosition!!]
         val mNumber = number?.text.toString().trim()
         val mEmail = email?.text.toString()
         val mPassword = password?.text.toString()
--- app/src/main/java/com/example/growup/SettingsActivity.kt
+++ app/src/main/java/com/example/growup/SettingsActivity.kt
@@ -2,11 +2,24 @@ package com.example.growup
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 
 class SettingsActivity : AppCompatActivity() {
+
+    private var backButton: FloatingActionButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        init()
+    }
+
+    private fun init(){
+        backButton = findViewById(R.id.back_button)
+
+        backButton?.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
